866b6fd4927b2a95acf34b5097e9687e5bfa4640
pikaiscoming <a129254770@gmail.com>
2022-07-23 16:18:11 +0800

    ch22

---
+++ ch22/about.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+  <head>
+		<title>About for the spring garden</title>
+    <meta charset=utf-8>
+    <meta name="description"  content="About our bulbs, seeds and plants for the spring garden">
+    <meta name="keywords" content="bulbs, seeds, plants, spring bulbs, spring garden seeds, spring garden plants">
+    <link href="style-3d-gdn.css" rel="stylesheet" type="text/css">
+     <!--[if lte IE 8]>
+		<script src="html5.js">
+    </script>
+		<![endif]-->
+    </head>
+  <body>
+    <div id="wrapper">
+      <?php include ("header.html"); ?>
+      <?php include ("nav.html"); ?>
+<div id="content">
+<div id="leftcol">
+      <?php include ("vertnav.html"); ?>
+</div>
+        <div id="rightcol">
+          <p>This is the far right column</p>
+        </div>
+        <div id="midcol">
+          <h2>About The Spring Garden Company</h2>
+          <div id="midcol-left">
+            <p class="left">View and order spring Plants using this website, or visit the Spring Garden Centre at 10 The Street, Townsville.</p><br>
+			<span class="quote">From "Spring" by
+			Thomas Nash</span><br>
+			  <p class="left">Nothing heralds the arrival of spring better than
+			  a garden full of spring flowers. This web site will help you to
+			  plan your spring flower display and choose the best bulbs and
+			  plants,</p>
+        <p>View and order items using this web site or visit the Garden
+			Center at 10 The Street, Townsville.</p>
+          </div>
+           <div id="midcol-right">
+			  <img alt="Daffodils" title="Daffodils" src="images/daffs-372x304-1.jpg" width="372" height="304">
+			  </div><br class="clear">
+      </div>
+    </div><!--content div finishes here-->
+      <br class="clear">
+      <? php include ("footer.html"); ?>
+    </div><!--wrapper div finishes here-->
+
+ </body>
+</html>
