c2eba4711d259efd09cdf09a404f8c3c5acccbc0
pikaiscoming <a129254770@gmail.com>
2022-07-29 14:27:31 +0800

    網站的東西

--- ch23/form-handler.php
+++ ch23/form-handler.php
@@ -1,41 +1,41 @@
 <?php
-//Feedback form-handler// 
-// set the website owner's email address 
-$mailto = "owners-address@example.co.uk";
-// set the web designers email address for test purposes 
+//Feedback form-handler//
+// set the website owner's email address
+$mailto = "a129254770@gmial.com";
+// set the web designers email address for test purposes
 //$mailto = "designers-address@example.co.uk";
-$subject = "Feedback from The Spring Garden website"; 
-// list the pages to be displayed, 
+$subject = "Feedback from The Spring Garden website";
+// list the pages to be displayed,
 $formurl = "http://localhost/Chapter-23/contact.php" ;
-$errorurl = "http://localhost/Chapter-23/error.html" ; 
-$thankyouurl = "http://localhost/Chapter-23/thankyou.html" ; 
-$emailerrurl = "http://localhost/Chapter-23/emailerr.html" ; 
+$errorurl = "http://localhost/Chapter-23/error.html" ;
+$thankyouurl = "http://localhost/Chapter-23/thankyou.html" ;
+$emailerrurl = "http://localhost/Chapter-23/emailerr.html" ;
 $errorphoneurl = "http://localhost/Chapter-23/phonerror.html" ;
 $errormessageurl = "http://localhost/Chapter-23/messagerror.html" ;
 $errorboxurl = "http://www.localhost/Chapter-23/boxerror.html" ;
 
 $uself = 0;
-// ------- Set the information received from the form as $ values --------------- 
-$headersep = (!isset( $uself ) || ($uself == 0)) ? "\r\n" : "\n" ; 
-$username = $_POST['username'] ; 
-$useremail = $_POST['useremail'] ; 
-$userphone = $_POST['userphone']; 
-$bulbs = $_POST['bulbs']; 
-$seeds = $_POST['seeds']; 
+// ------- Set the information received from the form as $ values ---------------
+$headersep = (!isset( $uself ) || ($uself == 0)) ? "\r\n" : "\n" ;
+$username = $_POST['username'] ;
+$useremail = $_POST['useremail'] ;
+$userphone = $_POST['userphone'];
+$bulbs = $_POST['bulbs'];
+$seeds = $_POST['seeds'];
 $plants = $_POST['plants'];
 $contact=$_POST['contact'];
-$message = $_POST['message'] ; 
-$http_referrer = getenv( "HTTP_REFERER" ); 
-if (!isset($_POST['useremail'])) { 
+$message = $_POST['message'] ;
+$http_referrer = getenv( "HTTP_REFERER" );
+if (!isset($_POST['useremail'])) {
             header( "Location: $formurl" );
             exit;}
 //Check that all three essential fields are filled in
-if (empty($username) || empty($useremail) || empty($message)) { 
-header( "Location: $errorurl" ); 
+if (empty($username) || empty($useremail) || empty($message)) {
+header( "Location: $errorurl" );
 		exit;}
 //OPTIONAL FILTER FOR CHECK BOXES
-//if ((!$bulbs and !$seeds and !plants)) { 
-//header( "Location: $errorboxurl" ); 
+//if ((!$bulbs and !$seeds and !plants)) {
+//header( "Location: $errorboxurl" );
 //		exit;}
 
 //check that no urls have been inserted in the username text area
@@ -47,43 +47,43 @@ if (strpos ($userphone, '://')||strpos($userphone, 'www') !==false){
     header( "Location: $errorphoneurl" );
             exit;}
 // Remove all characters from the phone entry that are not numbers (digits)
-$userphone = preg_replace('/\D+/', '', ($userphone)); 
+$userphone = preg_replace('/\D+/', '', ($userphone));
 //check that no urls have been inserted in the message text area
 if (strpos ($message, '://')||strpos($message, 'www') !==false){
     header( "Location: $errormessageurl" );
             exit;}
-if ( preg_match( "[\r\n]", $username ) || preg_match( "[\r\n]", $useremail )) { 
-          header( "Location: $errorurl" ); 
-          exit;} 
+if ( preg_match( "[\r\n]", $username ) || preg_match( "[\r\n]", $useremail )) {
+          header( "Location: $errorurl" );
+          exit;}
 #remove any spaces from beginning  and end of email address
-$useremail = trim($useremail); 
-#Check for permitted email address patterns 
-$_name = "/^[-!#$%&\'*+\\.\/0-9=?A-Z^_`{|}~]+"; 
-$_host = "([-0-9A-Z]+\.)+"; 
-$_tlds = "([0-9A-Z]){2,4}$/i"; 
-if(!preg_match($_name."@".$_host.$_tlds,$useremail)) { 
-          header( "Location: $emailerrurl" ); 
-          exit;} 
-if(!$bulbs) {$bulbs = "No";} 
-if(!$seeds) {$seeds = "No";} 
+$useremail = trim($useremail);
+#Check for permitted email address patterns
+$_name = "/^[-!#$%&\'*+\\.\/0-9=?A-Z^_`{|}~]+";
+$_host = "([-0-9A-Z]+\.)+";
+$_tlds = "([0-9A-Z]){2,4}$/i";
+if(!preg_match($_name."@".$_host.$_tlds,$useremail)) {
+          header( "Location: $emailerrurl" );
+          exit;}
+if(!$bulbs) {$bulbs = "No";}
+if(!$seeds) {$seeds = "No";}
 if(!$plants) {$plants = "No";}
 if($contact !=null) {$contact = $contact;}
-$messageproper = 
-          "This message was sent from:\n" . 
-          "$http_referrer\n" . 
+$messageproper =
+          "This message was sent from:\n" .
+          "$http_referrer\n" .
           "------------------------------------------------------------\n" .
-          "Name of sender: $username\n" . 
-          "Email of sender: $useremail\n" . 
-          "Telephone No: $phone\n" . 
-          "bulbs?: $bulbs\n" . 
+          "Name of sender: $username\n" .
+          "Email of sender: $useremail\n" .
+          "Telephone No: $phone\n" .
+          "bulbs?: $bulbs\n" .
           "seeds?: $seeds\n" .
           "plants?: $plants\n" .
           "Contact?:$email\n" .
-          "Contact?;$phone\n" .		
-          "------------------------- MESSAGE -------------------------\n\n" . 
-          $message . 
-          "\n\n------------------------------------------------------------\n" ; 
-mail($mailto, $subject, $messageproper, "From: \"$username\" <$useremail>" ); 
-header( "Location: $thankyouurl" ); 
+          "Contact?;$phone\n" .
+          "------------------------- MESSAGE -------------------------\n\n" .
+          $message .
+          "\n\n------------------------------------------------------------\n" ;
+mail($mailto, $subject, $messageproper, "From: \"$username\" <$useremail>" );
+header( "Location: $thankyouurl" );
 exit;
 ?>
---
+++ script.js
@@ -0,0 +1,95 @@
+function delay(n) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, n * 1000);
+    });
+}
+
+async function myAsyncFunction() {
+    while (1) {
+        const washingMachine_box1 = document.getElementById('wash-box-1');
+        const washingMachine_1 = document.getElementById('washingMachine-1');
+        const shower_box1 = document.getElementById('bath-box-1');
+        const shower_1 = document.getElementById('shower-1');
+        const disabled_box1 = document.getElementById('dis-box-1');
+        const disabled_1 = document.getElementById('disabled-1');
+        const dryerMachine_box1 = document.getElementById('dry-box-1');
+        const dryerMachine_1 = document.getElementById('dryerMachine');
+
+        console.log("start to fetch data");
+        fetch('http://127.0.0.1:8000/dorm/getJsondata')
+            .then((response) => {
+                console.log("response.json()", response);
+                return response.json();
+            })
+            .then((data) => {
+                washingMachine_box1_usage = data[0]["fields"]['battery'];
+                washingMachine_1_usage = data[0]["fields"]['battery'];
+                shower_box1_usage = data[1]["fields"]['battery'];
+                shower_1_usage = data[1]["fields"]['battery'];
+                disabled_box1_usage = data[2]["fields"]['battery'];
+                disabled_1_usage = data[2]["fields"]['battery'];
+                dryerMachine_box1_usage = data[3]["fields"]['battery'];
+                dryerMachine_1_usage = data[3]["fields"]['battery'];
+
+                console.log("washingMachine_1_usage",washingMachine_1_usage);
+                console.log(shower_1_usage);
+                console.log(disabled_1_usage);
+                console.log(dryerMachine_1_usage);
+
+                if (parseInt(washingMachine_box1_usage) > 10) {
+                    washingMachine_box1.style.background = 'yellow';
+                    washingMachine_1.style.fill = '#4071F5';
+                } else {
+                    washingMachine_1.style.fill = 'white';
+                    washingMachine_box1.style.background = '#609755';
+                }
+
+                if (parseInt(shower_1_usage) > 10) {
+                    shower_1.style.opacity = 1;
+                    shower_box1.style.background = 'yellow';
+                } else {
+                    shower_1.style.opacity = 0.1;
+                    shower_box1.style.background = '#609755';
+                }
+
+                if (parseInt(disabled_1_usage) > 10) {
+                    disabled_1.style.opacity = 1;
+                    disabled_box1.style.background = 'yellow';
+                } else {
+                    disabled_1.style.opacity = 0.1;
+                    disabled_box1.style.background = '#609755';
+                }
+
+                if (parseInt(dryerMachine_1_usage) > 10) {
+                    dryerMachine_1.style.animation = 'dash 1.4s linear infinite';
+                    dryerMachine_box1.style.background = 'yellow';
+                } else {
+                    dryerMachine_box1.style.background = '#609755';
+                }
+            })
+            .catch((error) => {
+                console.log(`Error: ${error}`);
+            });
+
+
+        await delay(10);
+    }
+}
+
+myAsyncFunction();
+
+// API pratice
+// fetch("https://api.unsplash.com/photos/random/?client_id=28snvwYLK_PZOGpDBVrXCf62otQb0xR3RRmiToO6ggs")
+//     .then((res) => {
+//         const data = res.json();
+//         return data;
+//     })
+//     .then((data) => {
+//         console.log(data);
+//         for (let i = 0; i < data.length; i++) {
+//             let container = document.querySelector(".container");
+//             let img = document.createElement("img");
+//             img.setAttribute("src", data[i].urls.small);
+//             container.append(img);
+//         }
+//     });
