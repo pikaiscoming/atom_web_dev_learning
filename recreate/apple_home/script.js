638142b31a03240d7bf65d74cc41d9f8e22b4727
pikaiscoming <a129254770@gmail.com>
2022-10-07 20:37:52 +0800

    apple_home_recreate_gallery_moving_js

--- recreate/apple_home/script.js
+++ recreate/apple_home/script.js
@@ -15,4 +15,28 @@ one.addEventListener('click', () => {
     images.style.transitionDuration = "0.5s";
     images.style.transform = `translate(-${current_img*1280}px)`;
 });
+two.addEventListener('click', () => {
+    images.style.transitionDuration = "0.5s";
+    images.style.transform = `translate(-${current_img*1280}px)`;
+});
+three.addEventListener('click', () => {
+    images.style.transitionDuration = "0.5s";
+    images.style.transform = `translate(-${current_img*1280}px)`;
+});
+four.addEventListener('click', () => {
+    images.style.transitionDuration = "0.5s";
+    images.style.transform = `translate(-${current_img*1280}px)`;
+});
+five.addEventListener('click', () => {
+    images.style.transitionDuration = "0.5s";
+    images.style.transform = `translate(-${current_img*1280}px)`;
+});
+six.addEventListener('click', () => {
+    images.style.transitionDuration = "0.5s";
+    images.style.transform = `translate(-${current_img*1280}px)`;
+});
+seven.addEventListener('click', () => {
+    images.style.transitionDuration = "0.5s";
+    images.style.transform = `translate(-${current_img*1280}px)`;
+});
 }
