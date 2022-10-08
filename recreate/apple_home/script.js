74e46b2deb7b26738138ac544169e9e845804d03
pikaiscoming <a129254770@gmail.com>
2022-10-08 15:21:33 +0800

    apple_home_recreate_final_swiping_gallery

--- recreate/apple_home/script.js
+++ recreate/apple_home/script.js
@@ -6,37 +6,43 @@ const four = document.getElementById('img-4');
 const five = document.getElementById('img-5');
 const six = document.getElementById('img-6');
 const seven = document.getElementById('img-7');
-var images = document.querySelector(".TV-plus .gallery");
-console.log(one);
-let current_img = 1;
+const images = document.querySelector(".TV-plus .gallery");
+const buts = document.getElementsByClassName("dot-but");
 
 
 one.addEventListener('click', () => {
     images.style.transitionDuration = "0.5s";
-    images.style.transform = `translate(-${current_img*1280}px)`;
+    images.style.transform = `translate(-${0}px)`;
+    one.style.backgroundColor = "#707070";
 });
 two.addEventListener('click', () => {
     images.style.transitionDuration = "0.5s";
-    images.style.transform = `translate(-${current_img*1280}px)`;
+    images.style.transform = `translate(-${1*1280}px)`;
+    two.style.backgroundColor = "#707070";
 });
 three.addEventListener('click', () => {
     images.style.transitionDuration = "0.5s";
-    images.style.transform = `translate(-${current_img*1280}px)`;
+    images.style.transform = `translate(-${2*1280}px)`;
+    three.style.backgroundColor = "#707070";
 });
 four.addEventListener('click', () => {
     images.style.transitionDuration = "0.5s";
-    images.style.transform = `translate(-${current_img*1280}px)`;
+    images.style.transform = `translate(-${3*1280}px)`;
+    four.style.backgroundColor = "#707070";
 });
 five.addEventListener('click', () => {
     images.style.transitionDuration = "0.5s";
-    images.style.transform = `translate(-${current_img*1280}px)`;
+    images.style.transform = `translate(-${4*1280}px)`;
+    five.style.backgroundColor = "#707070";
 });
 six.addEventListener('click', () => {
     images.style.transitionDuration = "0.5s";
-    images.style.transform = `translate(-${current_img*1280}px)`;
+    images.style.transform = `translate(-${5*1280}px)`;
+    six.style.backgroundColor = "#707070";
 });
 seven.addEventListener('click', () => {
     images.style.transitionDuration = "0.5s";
-    images.style.transform = `translate(-${current_img*1280}px)`;
+    images.style.transform = `translate(-${6*1280}px)`;
+    seven.style.backgroundColor = "#707070";
 });
 }
