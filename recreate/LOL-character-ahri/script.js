07def14a04306fb8467ba867783fc4fefa877ef5
pikaiscoming <a129254770@gmail.com>
2022-11-04 22:05:06 +0800

    LOL-character-page-recreate-skin-gallery-finish

--- recreate/LOL-character-ahri/script.js
+++ recreate/LOL-character-ahri/script.js
@@ -1,48 +1,26 @@
 window.onload = function() {
-const ahri = document.getElementById('ahri');
-const dynasty = document.getElementById('dynasty');
-const midnight = document.getElementById('midnight');
-const foxfire = document.getElementById('foxfire');
-const popstar = document.getElementById('popstar');
-const academy = document.getElementById('academy');
-const star= document.getElementById('star-guardian');
-const arcade = document.getElementById('arcade');
-const images = document.querySelector(".skins-gallery .gallery .left-aside .select-skin ul");
+const backimage = document.getElementById('gallery');
+const images = document.querySelector(".skins-gallery #gallery .left-aside .select-skin ul");
+const btns = images.querySelectorAll('button');
+console.log(btns[0].id);
+var idlist = []
 
-ahri.addEventListener('click', () => {
-    images.style.transitionDuration = "0.5s";
-    images.style.transform = `translateY(-${0}px)`;
-});
-dynasty.addEventListener('click', () => {
-    images.style.transitionDuration = "0.5s";
-    images.style.transform = `translateY(-${120}px)`;
-});
-midnight.addEventListener('click', () => {
-    images.style.transitionDuration = "0.5s";
-    images.style.transform = `translateY(-${240}px)`;
-});
-foxfire.addEventListener('click', () => {
-    images.style.transitionDuration = "0.5s";
-    images.style.transform = `translateY(-${360}px)`;
-});
-popstar.addEventListener('click', () => {
-    images.style.transitionDuration = "0.5s";
-    images.style.transform = `translateY(-${480}px)`;
-});
-challenger.addEventListener('click', () => {
-    images.style.transitionDuration = "0.5s";
-    images.style.transform = `translateY(-${600}px)`;
-});
-academy.addEventListener('click', () => {
-    images.style.transitionDuration = "0.5s";
-    images.style.transform = `translateY(-${720}px)`;
-});
-arcade.addEventListener('click', () => {
-    images.style.transitionDuration = "0.5s";
-    images.style.transform = `translateY(-${840}px)`;
-});
-star.addEventListener('click', () => {
-    images.style.transitionDuration = "0.5s";
-    images.style.transform = `translateY(-${960}px)`;
-});
+for (var i = 0; i < btns.length; i++) {
+    idlist.push(btns[i].id);
 }
+console.log(idlist);
+function geturl(Id) {
+    var content = document.getElementById(Id).getElementsByTagName('img')[0];
+    return content.src;
+};
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+    backimage.style.backgroundImage = 'url('+ geturl(this.id) +')', 2;
+    images.style.transitionDuration = "0.4s";
+    var num = idlist.indexOf(this.id);
+    images.style.transform = `translateY(-${num*120}px)`;
+    });
+}
+
+}
+
